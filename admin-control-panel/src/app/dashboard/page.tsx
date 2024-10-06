96daddf66d000f22fbc280e7591e8ea6f508a834
Revanth Sallangula <theblazecode@gmail.com>
2024-10-06 13:27:41 +0530

    Dashboard init

---
+++ admin-control-panel/src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+const Dashboard: React.FC = () => {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <p>We call this, ze Dashboard!</p>
+        </div>
+    );
+};
+
+export default Dashboard;
